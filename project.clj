42464b1b7394623c9fa182f9e9b5c437b68321c0
Ian Eslick <eslick@alum.mit.edu>
2016-01-14 23:22:39 -0800

    Bump clojure

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/vitalreactor/nativestore"
   :license {:name "MIT License"
             :url "http://github.com/vitalreactor/derive/blob/master/LICENSE"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.228"]
                  [im.chit/purnam.native "0.4.3"]
                  [prismatic/schema "0.2.6"]
