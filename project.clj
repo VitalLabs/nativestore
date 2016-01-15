e988ad594ffbb3fac0423aad1c7d980b2bf716c6
Ian Eslick <eslick@alum.mit.edu>
2016-01-15 15:10:02 -0800

    Update derive dependency; bump snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.vitalreactor/nativestore "0.2.0-SNAPSHOT"
+(defproject com.vitalreactor/nativestore "0.2.1-SNAPSHOT"
   :description "A client-side, in-memory, indexed data store."
   :url "http://github.com/vitalreactor/nativestore"
   :license {:name "MIT License"
@@ -7,7 +7,7 @@
                  [org.clojure/clojurescript "1.7.228"]
                  [im.chit/purnam.native "0.4.3"]
                  [prismatic/schema "0.2.6"]
-                 [com.vitalreactor/derive "0.2.0-SNAPSHOT"]
+                 [com.vitalreactor/derive "0.2.1-SNAPSHOT"]
                  [org.clojure/tools.nrepl "0.2.4"]]
   :plugins [[lein-cljsbuild "1.0.3"]
             [com.cemerick/austin "0.1.4"]
